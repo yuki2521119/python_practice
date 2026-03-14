e50b336ca4c802215c2f69c14aaccf9bcb7667bc
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2026-03-14 20:14:37 +0900

    Creare scrayping.py

---
+++ scrayping.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup 
+
+url = "https://qiita.com/Octoparse_Japan/items/3a766a5615d82674b873"
+response = requests.get(url)
+html = response.text
+
+# print(response.status_code)
+# print(response.text[:500])
+
+soup = BeautifulSoup(html, "html.parser")
+title = soup.find("h1").text
+
+print("テスト")
